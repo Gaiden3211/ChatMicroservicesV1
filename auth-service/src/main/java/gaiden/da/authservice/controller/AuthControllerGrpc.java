b9a637ec17c984cb1dd68198cb6b910f8e38a7e6
Gaiden3211 <evgeniykompaniec2006@gmail.com>
2026-03-26 09:00:00 +0200

    feat: endpoint for test

--- auth-service/src/main/java/gaiden/da/authservice/controller/AuthControllerGrpc.java
+++ auth-service/src/main/java/gaiden/da/authservice/controller/AuthControllerGrpc.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthControllerGrpc {
 
     private final AuthServiceGrpc authService;
+
+    @GetMapping("/test-io")
+    public String testIo() throws InterruptedException {
+        Thread.sleep(200);
+        return "Done";
+    }
 
     @PostMapping("register")
     @Operation(summary = "Register")
